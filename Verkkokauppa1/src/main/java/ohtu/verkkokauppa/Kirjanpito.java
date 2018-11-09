3a96e34325356d58429fd1aef9cbd50d1ee3450f
Sami Kukkonen <sami@samikukkonen.fi>
2018-11-09 12:51:26 +0200

    Annotated verkkokauppa1

---
+++ Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Kirjanpito.java
@@ -0,0 +1,23 @@
+
+package ohtu.verkkokauppa;
+
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+
+@Component
+public class Kirjanpito {
+    private ArrayList<String> tapahtumat;
+
+    public Kirjanpito() {
+        tapahtumat = new ArrayList<String>();
+    }
+    
+    public void lisaaTapahtuma(String tapahtuma) {
+        tapahtumat.add(tapahtuma);
+    }
+
+    public ArrayList<String> getTapahtumat() {
+        return tapahtumat;
+    }       
+}
